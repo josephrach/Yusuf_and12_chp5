50e20c9b87ea962d1756275da1d15586e9909381
Sir Joseph <sirjosephholmes@gmail.com>
2022-04-11 13:17:05 +0700

    mau attact fragment ke main activity tp ada error di manifest

--- app/src/main/java/com/example/game_suit_chp4/MainActivity.kt
+++ app/src/main/java/com/example/game_suit_chp4/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.widget.ImageView
 import com.example.game_suit_chp4.databinding.ActivityMainBinding
+import com.example.game_suit_chp4.fragment.SplashScreen
 
 //import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,13 +16,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding =  ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
-        var game = GameSuit()
-
-        game.play()
+        attachSplashFragment()
     }
 
-
+    private fun attachSplashFragment(){
+        val splashFragment = SplashScreen()
+        val container = R.id.splash_fragment
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.add(container,splashFragment)
+        fragmentTransaction.commit()
+    }
 
 }
