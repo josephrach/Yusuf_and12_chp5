8676a64b18f6ce3220877f43a4e60a0460a939e0
Sir Joseph <sirjosephholmes@gmail.com>
2022-04-19 06:52:11 +0700

    ngebuat layout dialog hasil

---
+++ app/src/main/java/com/example/game_suit_chp4/activity/ResultDialogActivity.kt
@@ -0,0 +1,9 @@
+package com.example.game_suit_chp4.activity
+
+import android.app.Dialog
+import android.os.Parcel
+import android.os.Parcelable
+
+class ResultDialogActivity() : Dialog() {
+
+}
--- app/src/main/java/com/example/game_suit_chp4/activity/SplashScreenActivity.kt
+++ app/src/main/java/com/example/game_suit_chp4/activity/SplashScreenActivity.kt
@@ -1,6 +1,9 @@
 package com.example.game_suit_chp4.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.game_suit_chp4.R
@@ -11,7 +14,14 @@ class SplashScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_splash_screen)
 
+        val halIntent = Intent(this, HalamanMenuActivity::class.java)
+
         val toast = Toast.makeText(this, "ini splash screen", Toast.LENGTH_SHORT)
         toast.show()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(halIntent)
+        }, 3000)
+
     }
 }
