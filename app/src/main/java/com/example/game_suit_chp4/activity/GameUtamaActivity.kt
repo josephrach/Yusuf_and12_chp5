076f68c648621d562130116a4a1bdfe4fd1aee4b
Sir Joseph <sirjosephholmes@gmail.com>
2022-04-18 13:34:43 +0700

    rename view halaman utama

---
+++ app/src/main/java/com/example/game_suit_chp4/activity/GameUtamaActivity.kt
@@ -0,0 +1,16 @@
+package com.example.game_suit_chp4.activity
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.game_suit_chp4.R
+
+class GameUtamaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_game_utama)
+
+        val batuP = findViewById<View>()
+    }
+
+}
